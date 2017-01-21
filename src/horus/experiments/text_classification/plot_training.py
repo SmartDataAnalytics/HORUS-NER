969be890ec7b3f46210e10bcff0ae44264e9d269
Diego Esteves <diegoesteves@gmail.com>
2017-01-21 01:12:04 +0100

    ok dbpedia

---
+++ src/horus/experiments/text_classification/plot_training.py
@@ -0,0 +1,47 @@
+import string
+
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.externals import joblib
+
+r = joblib.load('text_classification_results.pkl')
+
+results = []
+i = 0
+code = {'SGDClassifier': 'SGD', 'RandomForestClassifier': 'Random Forest',
+        'KNeighboarsClassifier': 'K-Neighboars', 'PassiveAggressiveClassifier': 'Passive Aggressive',
+        'RidgeClassifier': 'Ridge'}
+#trans = string.maketrans(*["".join(x) for x in zip(*code.items())])
+
+for temp in r:
+    if i not in (13, 9, 5):
+        clas = temp[0].replace('Classifier', '')
+        results.append([clas, temp[1], temp[2], temp[3]])
+    i+=1
+
+indices = np.arange(len(results))
+results = [[x[i] for x in results] for i in range(4)]
+clf_names, score, training_time, test_time = results
+training_time = np.array(training_time) / np.max(training_time)
+test_time = np.array(test_time) / np.max(test_time)
+
+plt.figure(figsize=(12, 8))
+plt.title("Text Classification: Test Performance (DBPedia - 30K abstracts)", fontweight="bold")
+plt.barh(indices, score, .4, label="F-measure", color="blue")
+#plt.barh(indices + .3, training_time, .2, label="Training Time")
+plt.barh(indices + .6, test_time, .4, label="Test Time", color="red", edgecolor = "black")
+plt.yticks(())
+plt.legend(loc='best')
+plt.subplots_adjust(left=.25)
+plt.subplots_adjust(top=.95)
+plt.subplots_adjust(bottom=.05)
+
+for i, c in zip(indices, clf_names):
+    plt.text(-.2, i, c, color='black', fontweight='bold')
+
+for i, c in zip(indices, score):
+    plt.text(c+.02, i, "%.2f" % round(c, 2), color='red')
+
+
+plt.show()
+exit(0)
--- src/horus/experiments/text_classification/textclassification_final.py
+++ src/horus/experiments/text_classification/textclassification_final.py
@@ -5,7 +5,6 @@ from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import RidgeClassifier
 from sklearn.linear_model import SGDClassifier
-import matplotlib.pyplot as plt
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
@@ -99,8 +98,8 @@ def benchmark(clf):
     test_time = time() - t0
     print("test time:  %0.3fs" % test_time)
 
-    score = metrics.accuracy_score(y_test, pred)
-    print("accuracy:   %0.3f" % score)
+    score = metrics.f1_score(y_test, pred)
+    print("f1:   %0.3f" % score)
 
     if hasattr(clf, 'coef_'):
         print("dimensionality: %d" % clf.coef_.shape[1])
@@ -164,27 +163,4 @@ results.append(benchmark(Pipeline([
   ('classification', LinearSVC())
 ])))
 
-indices = np.arange(len(results))
-
-results = [[x[i] for x in results] for i in range(4)]
-
-clf_names, score, training_time, test_time = results
-training_time = np.array(training_time) / np.max(training_time)
-test_time = np.array(test_time) / np.max(test_time)
-
-plt.figure(figsize=(12, 8))
-plt.title("Score")
-plt.barh(indices, score, .2, label="score", color='navy')
-plt.barh(indices + .3, training_time, .2, label="training time",
-         color='c')
-plt.barh(indices + .6, test_time, .2, label="test time", color='darkorange')
-plt.yticks(())
-plt.legend(loc='best')
-plt.subplots_adjust(left=.25)
-plt.subplots_adjust(top=.95)
-plt.subplots_adjust(bottom=.05)
-
-for i, c in zip(indices, clf_names):
-    plt.text(-.3, i, c)
-
-plt.show()
+joblib.dump(results, 'text_classification_results.pkl', compress=3)
