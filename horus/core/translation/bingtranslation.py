b17df9c27c6e5c63dc09ac88d8b4ac539d8d4e20
diegoesteves <nvclts@7>
2018-02-08 23:06:38 +0100

    IJCAI refactoring

--- horus/core/translation/bingtranslation.py
+++ horus/core/translation/bingtranslation.py
@@ -3,6 +3,8 @@ import urllib
 from urlparse import urlparse
 
 from xml.etree import ElementTree
+
+import re
 import requests
 from horus.core.config import HorusConfig
 from horus.core.translation.auth import AzureAuthClient
@@ -13,37 +15,51 @@ class BingTranslator(object):
     def __init__(self, config):
         auth_client = AzureAuthClient(config.translation_secret)
         self.final_token = 'Bearer ' + auth_client.get_access_token()
-        self.headers = {"Authorization ": self.final_token}
+        self.headers = {'Accept': 'application/xml'}
+        self.detectUrl = "https://api.microsofttranslator.com/V2/Http.svc/Detect"
+        self.translateUrl = "https://api.microsofttranslator.com/v2/Http.svc/Translate"
+
+    def clean_text(self, text):
+        return re.sub('\W+',' ', text)
+
     def detect_language(self, text):
         try:
+            text = self.clean_text(text)
             if len(str(text)) == 0:
                 return 'en'
-            detectUrl = "https://api.microsofttranslator.com/V2/Http.svc/Detect?text={}".format(text.encode('ascii','ignore'))
-            translationData = requests.get(detectUrl, headers=self.headers)
+            if isinstance(text, unicode) == True:
+                text = text.encode('ascii','ignore')
+            params = {'appid': self.final_token, 'text': text}
+            translationData = requests.get(self.detectUrl, params=params ,headers=self.headers)
             if translationData.status_code != 200:
-                raise Exception(':: error: bing lang detection status code: ' + str(translationData.status_code))
+                raise Exception(':: error: bing lang detection status code: ' + str(translationData.status_code) + ' - ' + str(translationData.text))
             translation = ElementTree.fromstring(translationData.text.encode('utf-8'))
             return translation.text
         except Exception as e:
              raise e
 
-    def translate(self, input_text, to_lang):
+    def translate(self, text, to_lang):
         try:
-            if len(str(input_text)) == 0:
+            text = self.clean_text(text)
+            if len(str(text)) == 0:
                 return ''
-            url = "http://api.microsofttranslator.com/v2/Http.svc/Translate"
-            params = {'to': to_lang, 'text': input_text.encode('ascii','ignore')}
-            translateUrl = "{}?{}".format(url, urllib.urlencode(params))
-            translationData = requests.get(translateUrl, headers=self.headers)
+            if isinstance(text, unicode) == True:
+                text = text.encode('ascii','ignore')
+
+            params = {'appid': self.final_token, 'text': text, 'to': to_lang}
+            translationData = requests.get(self.translateUrl, params=params, headers=self.headers) #urllib.urlencode()
             if translationData.status_code != 200:
-                raise Exception(':: error: bing translation status code: ' + str(translationData.status_code))
+                raise Exception(':: error: bing translation status code: ' + str(translationData.status_code) + ' - ' +
+                                str(translationData.text))
             translation = ElementTree.fromstring(translationData.text.encode('utf-8'))
             return translation.text
         except:
             raise
 
 if __name__ == "__main__":
-    t = BingTranslator()
+    config = HorusConfig()
+    t = BingTranslator(config)
     print t.translate("hey what's up dude?", 'pt-br')
     print t.detect_language("Que lingua estou falando, amigo?")
-    print t.detect_language("Claro que não, isso é portugues!")
+    print t.detect_language("Green Newsfeed")
+    print t.translate("Green Newsfeed", 'pt')
