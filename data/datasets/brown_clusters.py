a63743fa4c778d7d72ab875f7c8a04f7bff03f80
diegoesteves <nvclts@7>
2018-05-25 15:28:37 +0200

    - brown clusters - encoders

--- data/datasets/brown_clusters.py
+++ data/datasets/brown_clusters.py
@@ -1,20 +1,41 @@
-output_path='/Users/diegoesteves/Github/named-entity-recognition/horus-ner/data/datasets/'
-f_out = open(output_path + 'all_sentences.txt', 'w+')
-for file in ['Ritter/ner.txt',
-             'wnut/2015.conll.freebase',
-             'wnut/2016.conll.freebase',
-             'wnut/emerging.test.annotated']:
-    filepath= output_path + file
-    sentence=''
-    with open(filepath) as f:
-        content = f.readlines()
-        for x in content:
-            if x!='\n':
-                sentence+=x.split('\t')[0] + ' '
-            else:
-                f_out.write(sentence.strip() + '\n')
-                sentence=''
-    f_out.flush()
+import pickle
 
-f_out.close()
+from src.config import HorusConfig
 
+config = HorusConfig()
+
+def conll2sentence():
+    f_out = open(config.dir_datasets + 'all_sentences.txt', 'w+')
+    for file in ['Ritter/ner.txt',
+                 'wnut/2015.conll.freebase',
+                 'wnut/2016.conll.freebase',
+                 'wnut/emerging.test.annotated']:
+        filepath= config.dir_datasets + file
+        sentence=''
+        with open(filepath) as f:
+            content = f.readlines()
+            for x in content:
+                if x!='\n':
+                    sentence+=x.split('\t')[0] + ' '
+                else:
+                    f_out.write(sentence.strip() + '\n')
+                    sentence=''
+        f_out.flush()
+
+    f_out.close()
+
+def browncluster2dict():
+    try:
+        brown = dict()
+        with open(config.dir_datasets + 'output_brownclusters.txt') as f:
+            content = f.readlines()
+            for x in content:
+                n=x.split('\t')
+                brown.update({n[1]:str(n[0])})
+        with open(config.dir_datasets + 'browclustersdict.pkl', 'wb') as output:
+            pickle.dump(brown, output, pickle.HIGHEST_PROTOCOL)
+    except:
+        raise
+
+
+browncluster2dict()
