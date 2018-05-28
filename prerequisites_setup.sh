ce51b131295c107e52c24de8ffdba39027cbf39a
esteves <diegoesteves@gmail.com>
2018-05-28 09:58:37 +0200

    fix env

--- prerequisites_setup.sh
+++ prerequisites_setup.sh
@@ -2,4 +2,6 @@
 source activate horus_27_cv310
 pip install spacy && python -m spacy download en
 python download_models.py
+conda install -c conda-forge dlib=19.10
+conda install pytorch-cpu torchvision-cpu -c pytorch
 echo "horus pre-requisites done!"
