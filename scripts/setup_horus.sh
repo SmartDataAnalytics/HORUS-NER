a4331b1f9b0a32cb9df54aadf6dedcf2b35ad9e7
diego.esteves <diego.esteves@farfetch.com>
2020-04-29 18:49:13 +0100

    - clean up

--- scripts/setup_horus.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source activate horus
-pip install spacy && python -m spacy download en
-python download_models.py
-conda install -c conda-forge dlib=19.10
-conda install pytorch-cpu torchvision-cpu -c pytorch
-echo "horus pre-requisites done!"
